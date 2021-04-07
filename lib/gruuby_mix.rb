c1e5a39dbd8335569e4d9ef2c51ce752d0111f2d
ikaruga <ikaruga777@gmail.com>
2021-04-08 01:29:27 +0900

    Initial commit

---
+++ lib/gruuby_mix.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "gruuby_mix/version"
+
+module GruubyMix
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/gruuby_mix/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module GruubyMix
+  VERSION = "0.1.0"
+end
