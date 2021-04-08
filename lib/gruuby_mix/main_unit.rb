bf64c332b5776f71285ca4ea7099a5f2f411e33f
ikaruga <ikaruga777@gmail.com>
2021-04-08 09:38:28 +0900

    Add main unit

---
+++ lib/gruuby_mix/main_unit.rb
@@ -0,0 +1,15 @@
+module GruubyMix
+  class MainUnit
+    def initialize(members: [])
+      @members = members
+    end
+
+    def total
+      @members.sum { |member| member&.total.to_i }
+    end
+
+    def dj
+      @members.first
+    end
+  end
+end
