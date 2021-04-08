75a91d0bf878241f731c74494a06f6383a54e02b
ikaruga <ikaruga777@gmail.com>
2021-04-08 10:01:38 +0900

    Add member total test

--- test/gruuby_mix/member_test.rb
+++ test/gruuby_mix/member_test.rb
@@ -3,11 +3,18 @@
 require "minitest/autorun"
 
 class TestMember < Minitest::Test
-  def setup
+  def test_total
     @member = GruubyMix::Member.new(10_000, 2000, 300)
+    assert_equal 12_300, @member.total
   end
 
-  def test_total
-    assert_equal 12_300, @member.total
+  def test_total_zero
+    @member = GruubyMix::Member.new(0, 0, 0)
+    assert_equal 0, @member.total
+  end
+
+  def test_total_rate
+    @member = GruubyMix::Member.new(10_000, 2000, 300)
+    assert_equal 6150, @member.total(rate: 50)
   end
 end
