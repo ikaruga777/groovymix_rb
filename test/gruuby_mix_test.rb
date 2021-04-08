8e0bea84ea140417d6f4e162a6e4e749ebcfc097
ikaruga <ikaruga777@gmail.com>
2021-04-08 09:40:39 +0900

    Remove defualt failure test

--- test/gruuby_mix_test.rb
+++ test/gruuby_mix_test.rb
@@ -6,8 +6,4 @@ class GruubyMixTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::GruubyMix::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
