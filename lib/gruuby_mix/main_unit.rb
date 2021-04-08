c6ff8b27639621c8a22bc7de101c67e14d8fb8dd
ikaruga <ikaruga777@gmail.com>
2021-04-08 09:41:14 +0900

    rubocop -A

--- lib/gruuby_mix/main_unit.rb
+++ lib/gruuby_mix/main_unit.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module GruubyMix
   class MainUnit
     def initialize(members: [])
--- lib/gruuby_mix/sub_unit.rb
+++ lib/gruuby_mix/sub_unit.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module GruubyMix
   class SubUnit
     UNIT_RATE = 25
