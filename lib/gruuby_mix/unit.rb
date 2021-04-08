a453b1811adbafedce6d0bb0dbe241cef9280a71
ikaruga <ikaruga777@gmail.com>
2021-04-08 09:39:40 +0900

    Add unit

--- lib/gruuby_mix/unit.rb
+++ lib/gruuby_mix/unit.rb
@@ -2,5 +2,13 @@
 
 module GruubyMix
   class Unit
+    def initialize(main_unit:, sub_unit:)
+      @main_unit = main_unit
+      @sub_unit = sub_unit
+    end
+
+    def total
+      @main_unit.total + @sub_unit.total
+    end
   end
 end
