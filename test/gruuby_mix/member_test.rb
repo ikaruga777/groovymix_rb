b0947a281e264501e2738d770cae5ef0ce207dc6
ikaruga <ikaruga777@gmail.com>
2021-06-18 08:48:32 +0900

    テストなおす

--- test/gruuby_mix/member_test.rb
+++ test/gruuby_mix/member_test.rb
@@ -1,20 +1,22 @@
 # frozen_string_literal: true
 
 require "minitest/autorun"
+require './lib/gruuby_mix/member'
+require './lib/gruuby_mix/style'
 
 class TestMember < Minitest::Test
   def test_total
-    @member = GruubyMix::Member.new(10_000, 2000, 300)
+    @member = GruubyMix::Member.new(:test, 10_000, 2000, 300, GruubyMix::Style::COOL)
     assert_equal 12_300, @member.total
   end
 
   def test_total_zero
-    @member = GruubyMix::Member.new(0, 0, 0)
+    @member = GruubyMix::Member.new(:test, 0, 0, 0, GruubyMix::Style::COOL)
     assert_equal 0, @member.total
   end
 
   def test_total_rate
-    @member = GruubyMix::Member.new(10_000, 2000, 300)
+    @member = GruubyMix::Member.new(:test, 10_000, 2000, 300, GruubyMix::Style::COOL)
     assert_equal 6150, @member.total(rate: 50)
   end
 end
